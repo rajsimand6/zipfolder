0a0acf4c72d7f8167c5d80727a9dce8f034d7541
rajsimand6 <simandalahirajesh681@gmail.com>
2018-03-01 20:15:13 +0200

    add zip folder ability

--- zipfolder.rb
+++ zipfolder.rb
@@ -7,3 +7,13 @@ folder = "."
 input_filenames = ['Test.txt']
 
 zipfile_name = "./Test.zip"
+
+Zip::File.open(zipfile_name, create: true) do |zipfile|
+  input_filenames.each do |filename|
+    # Two arguments:
+    # - The name of the file as it will appear in the archive
+    # - The original file, including the path to find it
+    zipfile.add(filename, File.join(folder, filename))
+  end
+  zipfile.get_output_stream("myFile") { |f| f.write "myFile contains just this" }
+end
